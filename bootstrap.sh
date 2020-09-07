61ef2df2e8220ff2500e7736ad3fde36f20a7a79
Josh <joshtbernstein@gmail.com>
2020-09-07 14:54:42 -0700

    Add brew🍺

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,8 @@
 #
 echo "Installing Command Line Tools (CLT) for Xcode..."                                     
 xcode-select --install &>/dev/null
+
+# TODO: https://github.com/alrra/dotfiles/blob/main/src/os/install/macos/homebrew.sh
+#
+echo "Installing homebrew..."
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | sh
